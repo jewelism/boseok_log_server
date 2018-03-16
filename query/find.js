b23783f57ce1cf6b2e2d020b332d2266407b8b38
jewelism <boseokjung@gmail.com>
2018-03-16 18:21:43 +0900

    can filter by articles_all cache to return single id row (commented), fix id validate bug

--- query/find.js
+++ query/find.js
@@ -2,20 +2,32 @@ var pool = require('../config/db')
 const ResBody = require('../models/ResBody')
 const Cache = require('../cache')
 
-function find(res, cacheUri, query){
+function find(res, cacheUri, query, id) {
   pool.getConnection(function (err, connection) {
-    if(err){
+    if (err) {
       res.status(500).send('database connection failed');
       return false
     }
-  
-    if(Cache[cacheUri]){ 
+
+    // if (id && Cache.articles_all) { //get one by id from articles_all cache
+    //   const beforeTime = Date.now()
+    //   const data = Cache.articles_all.filter((el) => {
+    //     return el.id == id
+    //     // return el.id === parseInt(id)
+    //   })
+    //   const afterTime = Date.now()
+    //   console.log('return cache filter by id from articles_all :', id)
+    //   res.status(200).send(new ResBody(true, data, `article ${id} found successfully in cache (articles_all)`))
+    //   return true
+    // }
+
+    if (Cache[cacheUri]) {
       //return cache
-      res.status(200).send(new ResBody(true, Cache[cacheUri], 'article found successfully in cache'))
-      console.log('return cache', cacheUri)
+      console.log('return cache :', cacheUri)
+      res.status(200).send(new ResBody(true, Cache[cacheUri], `article found successfully in cache (${cacheUri})`))
       return true
     }
-  
+
     try {
       connection.query(query, function (err, rows) {
         if (err) {
@@ -24,7 +36,7 @@ function find(res, cacheUri, query){
           return false
         }
         console.log('return db rows')
-        if(cacheUri)
+        if (cacheUri)
           Cache[cacheUri] = rows //store in cache
         res.status(200).send(new ResBody(true, rows, 'article found successfully in DB'))
         return true
--- routes/articles/find.js
+++ routes/articles/find.js
@@ -9,23 +9,24 @@ const find = require('../../query/find')
 //   next();
 // });
 
-router.get('/all', (req, res, next) => {
-  const query = 'SELECT * from articles'
-  const cacheUri = 'articles_all'
-  find(res, cacheUri, query);
-})
-
 router.get('/version', (req, res, next) => {
   const query = 'SELECT * from article_version'
   find(res, null, query);
 })
 
+const cacheUri = 'articles_all'
+router.get('/all', (req, res, next) => {
+  const query = 'SELECT * from articles'
+  find(res, cacheUri, query);
+})
+
 /* GET One from articles. */
 router.get('/:id', (req, res, next) => {
   const { id } = req.params
-  if (Number.isInteger(parseInt(id))) {
+  if (parseInt(id)) {
     query = `SELECT * from articles where id=${id}`
-    find(res, null, query);
+    find(res, `article_id${id}`, query); //id cache
+    // find(res, null, query, id); //no id cache
   } else {
     res.status(400).send(new ResBody(false, null, 'invalid param, only Integer'))
   }
