e586d5a842de1a8f6f4840ea2c20ba4db5771302
jewelism <boseokjung@gmail.com>
2018-07-08 13:02:42 +0900

    fix routes

--- routes/files/routes.js
+++ routes/files/routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const findFile = require('./find');
-router.use('/', findFile);
+// const findFile = require('./find');
+// router.use('/', findFile);
 
 const saveFile = require('./save');
 router.use('/', saveFile);
 
-const deleteFile = require('./delete');
-router.use('/', deleteFile);
+// const deleteFile = require('./delete');
+// router.use('/', deleteFile);
 
 module.exports = router;
